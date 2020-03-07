8866228cd35c620d258f2c188387fea205e74d49
Diego Salazar <diego@Diegos-MacBook-Air.local>
2020-03-07 16:50:47 -0300

    WIP: Fixing scrapper status code errors

--- examples/simple-express-server/index.js
+++ examples/simple-express-server/index.js
@@ -3,30 +3,39 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
+const debug = require('debug')('simple-express-server');
+
 const cors = require('cors');
 
 app.use(cors());
 app.options('*', cors());
 
-const stare = require('../..')();
+const myMetrics = {
+  a: require('./my-metrics/a'),
+  b: require('./my-metrics/b')
+};
+
+const stare = require('../..')({
+  tempFilesPath: './my-personal-temp',
+  personalMetrics: myMetrics
+});
 
 app.get('/:engine', (request, response) => {
   let engine = request.params.engine;
   let { query, pageNumber } = request.query;
 
-  // let metrics = ['ranking', 'length'];
-  let metrics = ['language', 'length', 'perpiscuity', 'ranking'];
+  let metrics = ['language', 'ranking', 'language', 'perspicuity', 'a', 'b'];
 
   stare(engine, query, pageNumber, metrics)
     .then(result => {
       response.json(result);
     })
     .catch(err => {
-      console.log(`Error: %o`, err);
+      debug(`Error: %O`, err);
       response.status(500).json(err);
     });
 });
 
-app.listen(process.env.BACKEND_PORT, () => {
-  console.log(`simple-express-server app listening on port http://localhost:${process.env.BACKEND_PORT}!`);
+app.listen(process.env.SERVER_PORT, () => {
+  debug(`simple-express-server app listening on port http://localhost:${process.env.SERVER_PORT}!`);
 });
---
+++ examples/simple-express-server/my-metrics/b.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Calculates the length (number of chars) of the document.
+ * @param {Object} stareDocument - The Document data with stare format
+ * @param {string} stareDocument.title - Document title
+ * @param {string} stareDocument.link - Document link
+ * @param {string} stareDocument.snippet - Document snippet
+ * @param {string} stareDocument.image - Document image
+ * @param {Object} opts - Optional parameters to calculate the metric 
+ * @param {Object} opts.searchInfo - Search info for the query associated to the document
+ * @param {string} opts.searchInfo.totalResults - Total documents that generates the query (formatted with dots '.' in the thousands)
+ * @param {string} opts.searchInfo.searchTerms - Query string
+ * @param {integer} opts.searchInfo.numberOfItems - Number of documents that contains this page result
+ * @param {integer} opts.searchInfo.startIndex - Start document ranking for this page result.
+ * @returns {Promise}
+ */
+function calculate(stareDocument, opts) {
+  return new Promise((resolve, reject) => {
+    resolve({
+      name: 'b',
+      index: opts.index,
+      value: 0
+    });
+  });
+};
+
+module.exports = exports = calculate;

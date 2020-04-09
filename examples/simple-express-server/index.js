5d87f8cecb7851e816abc70c59a823809ce671d0
Diego Salazar <diego@Diegos-MacBook-Air.local>
2020-04-08 21:34:39 -0400

    ADD: Agrega dependecias de SERP para Google y Bing a examples

--- examples/simple-express-server/index.js
+++ examples/simple-express-server/index.js
@@ -1,10 +1,7 @@
+const debug = require('debug')('simple-express-server');
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
-
-const debug = require('debug')('simple-express-server');
-
+require('dotenv').config();
 const cors = require('cors');
 
 app.use(cors());

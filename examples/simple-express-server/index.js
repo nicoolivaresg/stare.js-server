387aa35c163e871a635cd65a04bb138a1edd3d74
Nicolas Olivares <nicolas.olivares.g@usach.cl>
2021-02-03 19:51:45 -0300

    ADD: Courts metric with sentences info, dates.

--- examples/simple-express-server/index.js
+++ examples/simple-express-server/index.js
@@ -36,7 +36,7 @@ app.get('/:engine', (request, response) => {
   let { query, numberOfResults } = request.query;
 
   // let metrics = [];
-  let metrics = ['lawsuit-ammount'];
+  let metrics = ['lawsuit-ammount', 'courts'];
   // let metrics = ['keywords-position', 'language', 'length', 'links', 'multimedia', 'perspicuity', 'ranking'];
   stare(engine, query, numberOfResults, metrics)
     .then(result => response.status(200).json(result))

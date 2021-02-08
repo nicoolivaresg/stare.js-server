69a603d88277d081b9e1fff07fd4a39988e57b30
Nicolas Olivares <nicolas.olivares.g@usach.cl>
2021-02-08 17:09:42 -0300

    ADD: profiles metrics

--- examples/simple-express-server/index.js
+++ examples/simple-express-server/index.js
@@ -36,8 +36,8 @@ app.get('/:engine', (request, response) => {
   let { query, numberOfResults } = request.query;
 
   // let metrics = [];
-  // let metrics = ['lawsuit-ammount', 'courts','injuries'];
-  let metrics = [];
+  // let metrics = ['lawsuit-ammount', 'courts','injuries']; //BAREMO
+  let metrics = ['profiles']; // SENCE
   // let metrics = ['keywords-position', 'language', 'length', 'links', 'multimedia', 'perspicuity', 'ranking'];
   stare(engine, query, numberOfResults, metrics)
     .then(result => response.status(200).json(result))

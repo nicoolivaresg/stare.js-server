db700ecd235ce56dc1d35f456953edb34bd0bdfa
Nicolas Olivares <nicolas.olivares.g@usach.cl>
2021-02-15 18:38:47 -0300

    UPD: package install

--- examples/simple-express-server/index.js
+++ examples/simple-express-server/index.js
@@ -19,7 +19,7 @@ const mySERPs = {
 };
 
 const stare = require('../..')({
-  engines: ['sence'],
+  engines: ['sence','baremo'],
   personalMetrics: myMetrics,
   personalSERPs: mySERPs,
   // google: {
@@ -36,8 +36,8 @@ app.get('/:engine', (request, response) => {
   let { query, numberOfResults } = request.query;
 
   // let metrics = [];
-  //let metrics = ['lawsuit-ammount', 'courts','injuries','accused-company'];
-  //let metrics = ['profiles', 'needed-elements']; // SENCE
+  let metrics = ['lawsuit-ammount', 'courts','injuries','accused-company'];
+  // let metrics = ['profiles', 'needed-elements']; // SENCE
   // let metrics = ['keywords-position', 'language', 'length', 'links', 'multimedia', 'perspicuity', 'ranking'];
   stare(engine, query, numberOfResults, metrics)
     .then(result => response.status(200).json(result))
